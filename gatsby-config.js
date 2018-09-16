c496ed239ef2a4fdc228ef6346fafd4bb200e667
sanketgandhi <sanketgandhi876@gmail.com>
2018-09-16 23:00:01 +0530

    Release 1.0.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -172,7 +172,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-65375615-1',
+        trackingId: 'UA-106405877-2',
       },
     },
     {
